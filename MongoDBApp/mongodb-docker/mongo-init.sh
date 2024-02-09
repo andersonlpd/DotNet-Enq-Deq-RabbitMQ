edddac1fb1d5be632e9823ef55f2cb1aec7a69b8
Anderson Dorigão <andersonlpd@gmail.com>
2024-02-09 12:43:45 -0300

    Corrigindo autenticação de usuário do mongo

--- MongoDBApp/mongodb-docker/mongo-init.sh
+++ MongoDBApp/mongodb-docker/mongo-init.sh
@@ -1,12 +1,12 @@
 set -e
 
 mongosh -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD <<EOF
-    db = db.getSiblingDB('rabbit')
+    db = db.getSiblingDB('$MONGO_INITDB_ROOT_DATABASE')
 
     db.createUser({
-        user: 'rabbit',
-        pwd: '$RABBIT_PASSWORD',
-        roles: [{ role: 'readWrite', db: 'rabbit' }],
+        user: '$APP_USERNAME',
+        pwd: '$APP_PASSWORD',
+        roles: [{ role: 'readWrite', db: '$MONGO_INITDB_ROOT_DATABASE' }],
     });
-    db.createCollection('messages')
+    db.createCollection('$APP_COLLECTION')
 EOF
